3844349e7f0db5caec09c5c09028293c44ccc43e
hi33741 <hi33741@naver.com>
2022-06-08 13:26:38 +0900

    제네릭

--- generic/test1.ts
+++ generic/test1.ts
@@ -1,6 +1,35 @@
+/**
+ * 함수의 기능은 똑같은데, 매개변수의 타입과 반환하는 타입이 다르다는 이유로 여러개의 함수를 구현할때
+ * 이러한 방식은 제네릭 기법을 사용하여 한 개의 함수로 구현할 수 있습니다.
+ */
+
+
+
 // function identity<T>(arg: T): T {
 //     return arg;
 //   }
+
+function NumberReturnFunc(arg: number): number {
+  return arg;
+}
+
+// string 타입의 매개변수를 return하는 함수
+function StringReturnFunc(arg: string): string {
+  return arg;
+}
+
+// boolean 타입의 매개변수를 return하는 함수
+function BooleanReturnFunc(arg: boolean): boolean {
+  return arg;
+}
+
+function GenericReturnFunc<T>(arg: T): T {
+  return arg;
+}
+
+
+
+console.log(GenericReturnFunc<number>(1))
 
 
   console.log(identity('124'))
